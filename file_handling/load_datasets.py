71bb744e1879135bb106ea07930c288835d4b5bf
Nathan Blair <nblair@berkeley.edu>
2019-03-12 20:24:25 -0700

    noise models

--- file_handling/load_datasets.py
+++ file_handling/load_datasets.py
@@ -7,30 +7,30 @@ import pickle
 
 def load_mnist(mode="numpy"):
 
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+	(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-    if mode == "numpy":
-        X_train = x_train.reshape((x_train.shape[0], -1))
-        X_test = x_test.reshape((x_test.shape[0], -1))
-        Y_train = np.eye(10)[y_train]
-        Y_test = np.eye(10)[y_test]
-        return (X_train, Y_train), (X_test, Y_test)
-    elif mode == "torch":
-        X_train = np.expand_dims(x_train, 1)
-        X_test = np.expand_dims(x_test, 1)
-        y_train = y_train
-        y_test = y_test
-        # X_test = np.expand_dims(X_test, 1)
-        # X_test = np.expand_dims(X_test, 1)
-        # print(X_train.shape)
-        tensor_x_train = torch.stack([torch.Tensor(x) for x in X_train])
-        tensor_y_train = torch.Tensor(y_train).long()
+	if mode == "numpy":
+		X_train = x_train.reshape((x_train.shape[0], -1))
+		X_test = x_test.reshape((x_test.shape[0], -1))
+		Y_train = np.eye(10)[y_train]
+		Y_test = np.eye(10)[y_test]
+		return (X_train, Y_train), (X_test, Y_test)
+	elif mode == "torch":
+		X_train = np.expand_dims(x_train, 1)
+		X_test = np.expand_dims(x_test, 1)
+		y_train = y_train
+		y_test = y_test
+		# X_test = np.expand_dims(X_test, 1)
+		# X_test = np.expand_dims(X_test, 1)
+		# print(X_train.shape)
+		tensor_x_train = torch.stack([torch.Tensor(x) for x in X_train])
+		tensor_y_train = torch.Tensor(y_train).long()
 
-        tensor_x_test = torch.stack([torch.Tensor(x) for x in X_test])
-        tensor_y_test = torch.Tensor(y_test).long()
+		tensor_x_test = torch.stack([torch.Tensor(x) for x in X_test])
+		tensor_y_test = torch.Tensor(y_test).long()
 
-        train_loader = TensorDataset(tensor_x_train,tensor_y_train)
-        test_loader = TensorDataset(tensor_x_test,tensor_y_test)
+		train_loader = TensorDataset(tensor_x_train,tensor_y_train)
+		test_loader = TensorDataset(tensor_x_test,tensor_y_test)
 
 		train_loader = DataLoader(dataset=train, batch_size=64, shuffle=True, num_workers=2)
 		test_loader = DataLoader(dataset=test, batch_size=64, shuffle=False, num_workers=1)
